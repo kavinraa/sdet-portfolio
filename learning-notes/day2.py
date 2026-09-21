3d2b2a92f54e93319b13174d54129698288e3177
kavinraa <kavinraj.antony@gmail.com>
2026-09-21 15:13:24 +0530

    Aug 30 - Docker Compose multi-service setup, Valid Anagram and Best Time to Buy/Sell Stock

--- learning-notes/day2.py
+++ learning-notes/day2.py
@@ -1,26 +1,26 @@
-# # Day 2 
-# # Part 1 — Lists
-# tests = ["login", "logout", "signup", "payment", "search"]
+# Day 2 
+# Part 1 — Lists
+tests = ["login", "logout", "signup", "payment", "search"]
 
-# print(tests[0])
-# print(tests[-1])
-# print(tests[1:3])
+print(tests[0])
+print(tests[-1])
+print(tests[1:3])
 
-# tests.append("checkout")
-# tests.insert(0, "homepage")
-# tests.remove("logout")
+tests.append("checkout")
+tests.insert(0, "homepage")
+tests.remove("logout")
 
-# print(tests)
+print(tests)
 
-# print(f"total tests : {len(tests)}")
-# print(f"is 'login' in suite? {'login' in tests}")
+print(f"total tests : {len(tests)}")
+print(f"is 'login' in suite? {'login' in tests}")
 
-# scores = [88, 72, 95, 61, 80]
-# print(f"highest score : {max(scores)}")
-# print(f"average score : {sum(scores)/len(scores):.1f}")
+scores = [88, 72, 95, 61, 80]
+print(f"highest score : {max(scores)}")
+print(f"average score : {sum(scores)/len(scores):.1f}")
 
-# passed =[s for s in scores if s >= 80]
-# print(f"Passed scores : {passed}")
+passed =[s for s in scores if s >= 80]
+print(f"Passed scores : {passed}")
 
 
 def twoSum(nums, target):
@@ -45,5 +45,4 @@ def containsDuplicate(nums: list[int]) -> bool:
 # print(containsDuplicate([1,2,3,5]))
 # print(containsDuplicate([1, 2, 3, 1])) 
 
-print(containsDuplicate([])) 
 
